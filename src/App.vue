4087d4b236c9dd4bb63ac060face43743dede2a4
moonchoi-e2x <moon.choi@e2x.com>
2023-07-11 08:29:11 +0100

    Pinia Crash Course #4 - Accessing Store State

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <main>
+
+    <!-- heading -->
+    <header>
+      <img src="https://pinia.vuejs.org/logo.svg" alt="pinia logo">
+      <h1>Pinia Tasks</h1>
+    </header>
+    
+    <!-- task list -->
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks">
+        <TaskDetails :task="task"/>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+  import TaskDetails from './components/TaskDetails.vue'
+  import { useTaskStore } from './stores/TaskStore'
+
+  export default {
+    components: { TaskDetails },
+    setup() {
+      //access the store by invoking it.
+      const taskStore = useTaskStore() // invoked.
+
+      return { taskStore } // to enable it in the vue template.
+    }
+  }
+
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import './assets/main.css'
-import { createPinia } from 'pinia';
 import { createApp } from 'vue'
+import { createPinia } from 'pinia';
 import App from './App.vue'
+import './assets/main.css'
 
 createApp(App).use(createPinia()).mount('#app')
