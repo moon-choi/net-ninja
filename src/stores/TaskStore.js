182061949f0b6b92c2e059dd8b78139f1c077b5a
moonchoi-e2x <moon.choi@e2x.com>
2023-07-11 09:02:42 +0100

    Pinia Crash Course #5 - Getters WIP

--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -9,5 +9,16 @@ export const useTaskStore = defineStore('taskStore', {
       {id: 1, title: 'buy some milk', isFav: false},
       {id: 2, title: 'play Gloomhaven', isFav: true},
     ],
-  }) //return an object.
+  }), //return an object.
+  getters: {
+    favs() {
+      //filtered version - non destructive.
+      return this.tasks.filter(t => t.title)
+    }
+  }
 })
+
+
+//why create getter func?
+//package up so many manipulation logic.
+//gets tasks only the isFav true, or only returns titles. 
