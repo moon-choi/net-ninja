427c96476bdfe4aed615c2fa370f036c117611a2
moonchoi-e2x <moon.choi@e2x.com>
2023-07-12 08:42:56 +0100

    Pinia Crash Course #5 - Getters

--- src/App.vue
+++ src/App.vue
@@ -6,28 +6,44 @@
       <img src="https://pinia.vuejs.org/logo.svg" alt="pinia logo">
       <h1>Pinia Tasks</h1>
     </header>
-    
+
+    <!-- filter -->
+    <nav class="filter">
+      <button @click="filter = 'all'">All tasks</button>
+      <button @click="filter = 'favs'">Fav tasks</button>
+    </nav>
+
     <!-- task list -->
-    <div class="task-list">
-      <div v-for="task in taskStore.tasks">
-        <TaskDetails :task="task"/>
+    <div class="task-list" v-if="filter === 'all'">
+      <p>You have {{ taskStore.totalCount }} tasks left to do.</p>
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <TaskDetails :task="task" />
       </div>
     </div>
+    <div class="task-list" v-if="filter === 'favs'">
+      <p>You have {{ taskStore.favCount }} tasks in your favs list.</p>
+      <div v-for="task in taskStore.favs" :key="task.id">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+
   </main>
 </template>
 
 <script>
   import TaskDetails from './components/TaskDetails.vue'
+
   import { useTaskStore } from './stores/TaskStore'
+  import { ref } from 'vue'
 
   export default {
     components: { TaskDetails },
     setup() {
-      //access the store by invoking it.
-      const taskStore = useTaskStore() // invoked.
+      const taskStore = useTaskStore()
 
-      return { taskStore } // to enable it in the vue template.
+      const filter = ref('all')
+
+      return { taskStore, filter }
     }
   }
-
 </script>
--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -6,19 +6,28 @@ import { defineStore } from 'pinia'
 export const useTaskStore = defineStore('taskStore', {
   state: () => ({
     tasks: [
-      {id: 1, title: 'buy some milk', isFav: false},
-      {id: 2, title: 'play Gloomhaven', isFav: true},
+      { id: 1, title: 'buy some milk', isFav: false },
+      { id: 2, title: 'play Gloomhaven', isFav: true },
     ],
   }), //return an object.
   getters: {
     favs() {
       //filtered version - non destructive.
       return this.tasks.filter(t => t.title)
+    },
+    favCount() {
+      return this.tasks.reduce((p, c) => {
+        return c.isFav ? p + 1 : p
+      }, 0)
+    },
+    totalCount: (state) => {
+      return state.tasks.length
     }
   }
 })
 
 
 //why create getter func?
+//getter gets a state.
 //package up so many manipulation logic.
 //gets tasks only the isFav true, or only returns titles. 
