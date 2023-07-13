2de8cecc9e3faefa14acf60453c19ac28a0bbc8c
moonchoi-e2x <moon.choi@e2x.com>
2023-07-13 08:57:27 +0100

    Pinia Crash Course #6 - Actions

--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -1,21 +1,16 @@
 import { defineStore } from 'pinia'
 
-//return value is usually a function itself. 
-//when you invoke it, typical naming convention is useSomething.
-
 export const useTaskStore = defineStore('taskStore', {
   state: () => ({
     tasks: [
-      { id: 1, title: 'buy some milk', isFav: false },
-      { id: 2, title: 'play Gloomhaven', isFav: true },
+      { id: 1, title: "buy some milk", isFav: false },
+      { id: 2, title: "play Gloomhaven", isFav: true },
       { id: 3, title: 'do the dishes', isFav: true },
       { id: 4, title: 'return amazon', isFav: true },
-
-    ],
-  }), //return an object.
+    ]
+  }),
   getters: {
     favs() {
-      //filtered version - non destructive.
       return this.tasks.filter(t => t.isFav)
     },
     favCount() {
@@ -26,11 +21,19 @@ export const useTaskStore = defineStore('taskStore', {
     totalCount: (state) => {
       return state.tasks.length
     }
+  },
+  actions: {
+    addTask(task) {
+      this.tasks.push(task)
+    },
+    deleteTask(id) {
+      this.tasks = this.tasks.filter(t => {
+        return t.id !== id
+      })
+    },
+    toggleFav(id) {
+      const task = this.tasks.find(t => t.id === id)
+      task.isFav = !task.isFav
+    }
   }
 })
-
-
-//why create getter func?
-//getter gets a state.
-//package up so many manipulation logic.
-//gets tasks only the isFav true, or only returns titles. 
